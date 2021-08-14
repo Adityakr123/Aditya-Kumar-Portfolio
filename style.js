3008cb25865a51dc556aa5a8f147f79244ef32d2
Aditya Kumar <71920316+Adityakr123@users.noreply.github.com>
2021-08-14 23:55:35 +0530

    Update style.js

--- style.js
+++ style.js
@@ -1,35 +1,35 @@
-var a= document.getElementById('adi');
-a.className="fadeinimg";
-var counter = 0;
-var myimages=["a.JPG","bnp.jpg","git.png","hacker.png","r.jpeg"];
-    // setInterval(function(){
-setInterval(function next()
-{
-      // document.getElementById('radio' + counter).checked = true;
-      var a= document.getElementById('adi');
-      a.className="fadeinimg";
-      counter++;
+// var a= document.getElementById('adi');
+// a.className="fadeinimg";
+// var counter = 0;
+// var myimages=["a.JPG","bnp.jpg","git.png","hacker.png","r.jpeg"];
+//     // setInterval(function(){
+// setInterval(function next()
+// {
+//       // document.getElementById('radio' + counter).checked = true;
+//       var a= document.getElementById('adi');
+//       a.className="fadeinimg";
+//       counter++;
       
-      if(counter > 4){
-        counter = 1;
-       a.src=myimages[counter];
+//       if(counter > 4){
+//         counter = 1;
+//        a.src=myimages[counter];
 
-      }
-      else{
-        a.src=myimages[counter];
-      }
-}, 5000);
-function next()
-{
-      // document.getElementById('radio' + counter).checked = true;
+//       }
+//       else{
+//         a.src=myimages[counter];
+//       }
+// }, 5000);
+// function next()
+// {
+//       // document.getElementById('radio' + counter).checked = true;
 
-      counter++;
+//       counter++;
       
-      if(counter > 4){
-        counter = 0;
-        a.src=myimages[counter];
-      }
-      else{
-        a.src=myimages[counter];
-      }
-}
+//       if(counter > 4){
+//         counter = 0;
+//         a.src=myimages[counter];
+//       }
+//       else{
+//         a.src=myimages[counter];
+//       }
+// }
