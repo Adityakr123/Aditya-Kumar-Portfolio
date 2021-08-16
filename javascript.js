b74eaa8b463340c7c8c22ee7054d72560d5ebb6d
Aditya Kumar <71920316+Adityakr123@users.noreply.github.com>
2021-08-16 11:41:10 +0530

    Update javascript.java

--- javascript.js
+++ javascript.js
@@ -1,38 +1,64 @@
-
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+var a= document.getElementById('adi');
+var counter = 0;
+var myimages=["photo.JPG","bnp.jpg","git.png","hacker.png","r.jpeg"];
+setInterval(function next()
+{
+     
+      counter++;
+      
+      if(counter > 4){
+        counter = 1;
+        
+        a.className="fadeinimg";
+        a.src=myimages[counter];
+        // document.getElementById('aditya').innerHTML="ADITYA";
+        
       }
-    }
-  }
-}
-
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function myFunction1() {
-  document.getElementById("myDropdown1").classList.toggle("show1");
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn1')) {
-    var dropdowns1 = document.getElementsByClassName("dropdown-content1");
-    var i;
-    for (i = 0; i < dropdowns1.length; i++) {
-      var openDropdown1 = dropdowns1[i];
-      if (openDropdown1.classList.contains('show1')) {
-        openDropdown1.classList.remove('show1');
+      else{
+        
+        a.className="fadeinimg";
+        a.src=myimages[counter];
+        // document.getElementById('aditya').innerHTML="ADITYA";
       }
-    }
-  }
+      
+}, 5000);
+function next()
+{
+
+      counter++;
+      
+      if(counter > 4){
+        counter = 0;
+        a.className="fadeinimg";
+        a.src=myimages[counter];
+        // document.getElementById('aditya').innerHTML="ADITYA";
+      }
+      else{
+        a.className="fadeinimg";
+        a.src=myimages[counter];
+        // document.getElementById('aditya').innerHTML="ADITYA";
+      }
 }
+function before()
+{
+
+      counter--;
+      
+      if(counter > 4){
+        counter = 0;
+        a.src=myimages[counter];
+        // document.getElementById('aditya').innerHTML="ADITYA";
+      }
+      else if(counter<0){
+        counter = 4;
+        a.src=myimages[counter];
+        // document.getElementById('aditya').innerHTML="ADITYA";
+      }
+      else{
+        a.src=myimages[counter];
+        var b="ADITYA KUMAR ";
+        b.className += "tex";
+        document.getElementsByClassName('text2').innerHTML=b;
+      }
+}
+
